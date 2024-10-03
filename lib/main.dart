67f1961efa1e4a9977c05a202ee7bb0e0369394d
miyamoto <smiyamoto.610@gmail.com>
2024-10-03 17:35:43 +0900

    feat: supabase の初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://kgadjovnnulfhsjdpolf.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnYWRqb3ZubnVsZmhzamRwb2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5NDM4MDEsImV4cCI6MjA0MzUxOTgwMX0.dwnYbzv_UooYoIWwjFCRYUSEn-3oLHDANWp8kaR1ORM',
+  );
+
   runApp(const MyApp());
 }
 
